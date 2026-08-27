f34f515bea0015c42f22b6597f9dacda6f9b57b6
Nidhish Agarwal <nidhishagarwal01@gmail.com>
2026-08-27 12:39:22 +0530

    feat(navigation): integrate teammate NavigationProvider & BackButton hierarchy into root tree

--- heritage-shield-ui/src/main.jsx
+++ heritage-shield-ui/src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ErrorBoundary from './components/ErrorBoundary.jsx'
 import { ThemeProvider } from './context/ThemeContext'
+import { NavigationProvider } from './context/NavigationContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <NavigationProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </NavigationProvider>
   </StrictMode>,
 )
 
